d870ac09a5e775343f45e2501afde7c227eddcee
Shannon <148235316+Yogurt44@users.noreply.github.com>
2025-02-18 12:00:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -63,5 +63,10 @@
                 audioPlayer.load();
             }
         }
+        function toggleTheme() {
+            document.body.classList.toggle("dark-mode");
+            localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
+        }
+
         
         
